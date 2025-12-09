5c4517f4bcaec1060b4f87defe8cc0de4807a0be
BsaLee <luqiao321@gmail.com>
2025-12-10 03:00:15 +0800

    修复参数保存不生效

--- app/src/main/java/com/frpc/launcher/ConfigConstants.java
+++ app/src/main/java/com/frpc/launcher/ConfigConstants.java
@@ -8,7 +8,7 @@ public class ConfigConstants {
     // 服务器配置默认值
     public static final String DEFAULT_SERVER_ADDR = "1.2.3.4";
     public static final int DEFAULT_SERVER_PORT = 1024;
-    public static final String DEFAULT_AUTH_TOKEN = "1024";
+    public static final String DEFAULT_AUTH_TOKEN = "12345";
     public static final int DEFAULT_LOCAL_PORT = 5555;
     
     // 随机端口范围默认值
